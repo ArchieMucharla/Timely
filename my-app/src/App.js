9c0ccaeff12b86635768d4422190e203dc831499
Emeraldb01 <naisyuanchang@gmail.com>
2025-04-21 18:59:47 -0500

    🐛 remove redundant references

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import EventsPage from './pages/EventsPage';
 import UserProfile from './pages/UserProfile';
-import MyEventsPage from './pages/MyEventsPage';
 import AdminDashboard from './pages/AdminDashboard';
-import EventViewPage from './pages/EventViewPage'; // optional
 import LoginPage from './pages/LoginPage';
 import CreateEventPage from './pages/CreateEventPage';
 
@@ -19,10 +17,8 @@ function App() {
           <Route path="/" element={<EventsPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<UserProfile />} />
-          <Route path="/my-events" element={<MyEventsPage />} />
           <Route path="/create_event" element={<CreateEventPage />} />
           <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/events/:id" element={<EventViewPage />} /> {/* optional */}
         </Routes>
       </div>
     </Router>
