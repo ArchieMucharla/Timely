3a6ded072d0bd91a539cd24d2e19bf9f6e8b90b0
Emeraldb01 <naisyuanchang@gmail.com>
2025-04-27 11:07:37 -0500

    🔧 Remove unused QRCodeCanvas import from EventsPage.js

--- my-app/src/pages/EventsPage.js
+++ my-app/src/pages/EventsPage.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CategoryFilter from '../components/CategoryFilter';
 import EventList from '../components/EventList';
-import { QRCodeCanvas } from 'qrcode.react';
 
 const BACKEND = 'http://localhost:5050';
 
